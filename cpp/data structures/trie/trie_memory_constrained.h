b0b744c91df01cb62611957fdccc0823486c3ca2
Leiko Ravelo <l.ravelo@samsung.com>
2019-08-07 10:26:48 +0800

    format trie

--- cpp/data structures/trie/trie_memory_constrained.h
+++ cpp/data structures/trie/trie_memory_constrained.h
@@ -3,64 +3,62 @@
 
 class Trie {
 private:
-    bool endOfChar;
-    int numChildren;
-    Trie* children[26];
+  bool endOfChar;
+  int numChildren;
+  Trie *children[26];
 
-    bool delete_child(char* keyword)
-    {
-        if (keyword[0] == '\0') return endOfChar;
+  bool delete_child(char *keyword) {
+    if (keyword[0] == '\0')
+      return endOfChar;
 
-        int index = (int)(keyword[0] -'a');
+    int index = (int)(keyword[0] - 'a');
 
-        if (children[index] == nullptr) return false;
-        if (children[index]->delete_child(keyword + 1))
-        {
-            delete children[index];
-            children[index] = nullptr;
-            numChildren--;
-        }
-
-        return numChildren <= 0;
+    if (children[index] == nullptr)
+      return false;
+    if (children[index]->delete_child(keyword + 1)) {
+      delete children[index];
+      children[index] = nullptr;
+      numChildren--;
     }
+
+    return numChildren <= 0;
+  }
+
 public:
-    Trie()
-    {
-        endOfChar = false;
-        for (int i=0; i<26; i++) children[i] = nullptr;
+  Trie() {
+    endOfChar = false;
+    for (int i = 0; i < 26; i++)
+      children[i] = nullptr;
+  }
+
+  void add(char *keyword) {
+    if (keyword[0] == '\0') {
+      endOfChar = true;
+      return;
     }
 
-    void add(char* keyword)
-    {
-        if (keyword[0] == '\0')
-        {
-            endOfChar = true;
-            return;
-        }
+    int index = (int)(keyword[0] - 'a');
 
-        int index = (int)(keyword[0] - 'a');
-
-        if (children[index] ==  nullptr)
-        {
-            children[index] = new Trie();
-            numChildren++;
-        }
-        children[index]->add(keyword + 1);
+    if (children[index] == nullptr) {
+      children[index] = new Trie();
+      numChildren++;
     }
+    children[index]->add(keyword + 1);
+  }
 
-    bool search(char* keyword)
-    {
-        if (keyword[0] == '\0') return endOfChar;
+  bool search(char *keyword) {
+    if (keyword[0] == '\0')
+      return endOfChar;
 
-        int index = (int)(keyword[0] - 'a');
+    int index = (int)(keyword[0] - 'a');
 
-        if (children[index] == nullptr) return false;
-        return children[index]->search(keyword + 1);
-    }
+    if (children[index] == nullptr)
+      return false;
+    return children[index]->search(keyword + 1);
+  }
 
-    void delete_key(char* keyword)
-    {
-        // Perform DFS for delete
-        delete_child(keyword);
-    }
+  void delete_key(char *keyword) {
+    // Perform DFS for delete
+    delete_child(keyword);
+  }
 };
