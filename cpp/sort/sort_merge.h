689a549b0e597eb5c1647880d2d1ed13a6ce27e9
Leiko Ravelo <l.ravelo@samsung.com>
2019-08-07 10:13:09 +0800

    format merge sort code

--- cpp/sort/sort_merge.h
+++ cpp/sort/sort_merge.h
@@ -1,65 +1,65 @@
 // Merge sort
 // Using function templates to use any data type
 
-template <class arrayType>
-void merge(arrayType* arr, arrayType* temp, int left, int middle, int right, bool (*cb)(arrayType, arrayType))
-{
-    int leftIndex;
-    int rightIndex;
-    int arrIndex;
+template <class type>
+void merge(type *arr, type *temp, int left, int middle, int right,
+           bool (*cb)(type, type)) {
+  int leftIndex;
+  int rightIndex;
+  int arrIndex;
 
-    // Copy left and right array to temp
-    for(leftIndex=left; leftIndex<=middle; leftIndex++) temp[leftIndex] = arr[leftIndex];
-    for(rightIndex=middle+1; rightIndex<=right; rightIndex++) temp[rightIndex] = arr[rightIndex];
+  // Copy left and right array to temp
+  for (leftIndex = left; leftIndex <= middle; leftIndex++)
+    temp[leftIndex] = arr[leftIndex];
+  for (rightIndex = middle + 1; rightIndex <= right; rightIndex++)
+    temp[rightIndex] = arr[rightIndex];
 
-    leftIndex = left;
-    rightIndex = middle+1;
-    arrIndex = left;
+  leftIndex = left;
+  rightIndex = middle + 1;
+  arrIndex = left;
 
-    // For each element in subarray, fill-in according to comparison function
-    while(leftIndex<=middle && rightIndex<=right)
-    {
-        if (cb(temp[leftIndex], temp[rightIndex])) arr[arrIndex++] = temp[leftIndex++];
-        else arr[arrIndex++] = temp[rightIndex++];
-    }
+  // For each element in subarray, fill-in according to comparison function
+  while (leftIndex <= middle && rightIndex <= right) {
+    if (cb(temp[leftIndex], temp[rightIndex]))
+      arr[arrIndex++] = temp[leftIndex++];
+    else
+      arr[arrIndex++] = temp[rightIndex++];
+  }
 
-    // Fill in remaining elements
-    while(leftIndex<=middle) arr[arrIndex++] = temp[leftIndex++];
-    while(rightIndex<=right) arr[arrIndex++] = temp[rightIndex++];
+  // Fill in remaining elements
+  while (leftIndex <= middle)
+    arr[arrIndex++] = temp[leftIndex++];
+  while (rightIndex <= right)
+    arr[arrIndex++] = temp[rightIndex++];
 }
 
+template <class type>
+void divideArray(type *arr, type *temp, int left, int right,
+                 bool (*cb)(type, type)) {
+  // Divide into subproblems
+  if (left >= right)
+    return;
 
-template <class arrayType>
-void divideArray(arrayType* arr, arrayType* temp, int left, int right, bool (*cb)(arrayType, arrayType))
-{
-    // Divide into subproblems
-    if (left >= right) return;
+  int middle = (left + right) / 2;
+  divideArray(arr, temp, left, middle, cb);
+  divideArray(arr, temp, middle + 1, right, cb);
 
-    int middle = (left + right) / 2;
-    divideArray(arr, temp, left, middle, cb);
-    divideArray(arr, temp, middle+1, right, cb);
-
-    merge(arr, temp, left, middle, right, cb);
+  merge(arr, temp, left, middle, right, cb);
 }
 
-template <class arrayType>
-bool compareClassic(arrayType left, arrayType right)
-{
-    // less than or EQUAL for stable sort
-    return left <= right;
+template <class type> bool compareClassic(type left, type right) {
+  // less than or EQUAL for stable sort
+  return left <= right;
 }
 
-template <class arrayType>
-void sort(arrayType* arr, int len)
-{
-    arrayType* temp = new arrayType[len];
-    divideArray(arr, temp, 0, len-1, compareClassic);
+template <class type> void sort(type *arr, int len) {
+  type *temp = new type[len];
+  divideArray(arr, temp, 0, len - 1, compareClassic);
 }
 
-template <class arrayType>
-void sort(arrayType* arr, int len, bool (*compare)(arrayType, arrayType))
-{
-    arrayType* temp = new arrayType[len];
-    divideArray(arr, temp, 0, len-1, compare);
-    delete temp;
+template <class type>
+void sort(type *arr, int len, bool (*compare)(type, type)) {
+  type *temp = new type[len];
+  divideArray(arr, temp, 0, len - 1, compare);
+  delete temp;
 }
